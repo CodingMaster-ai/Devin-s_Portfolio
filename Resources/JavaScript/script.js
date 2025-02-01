4535635de697c1afe6ab07bc2eb5e069d5f4ebbf
Devin <devinsloan2005@gmail.com>
2025-02-01 02:18:25 -0500

    responsive setup

--- Resources/JavaScript/script.js
+++ Resources/JavaScript/script.js
@@ -2,15 +2,15 @@
 let projectReveal = document.getElementById('button-1');
 let projectSection = document.getElementById('projects');
 
-// Show Skills
-let skillsReveal = document.getElementById('button-2');
-let skillsSection = document.getElementById('skills');
-
 function showProjects() {
     projectSection.style.display = 'block';
 }
 
 projectReveal.addEventListener('click', showProjects);
+
+// Show Skills
+let skillsReveal = document.getElementById('button-2');
+let skillsSection = document.getElementById('skills');
 
 function showSkills() {
     skillsSection.style.display = 'block';
